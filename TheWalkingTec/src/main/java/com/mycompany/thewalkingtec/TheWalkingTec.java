b3d4e9e2402d933537835ad8ee884aeafe39890e
avwong <alinarodriguezwong@gmail.com>
2025-10-06 11:38:26 -0600

    Test

---
+++ TheWalkingTec/src/main/java/com/mycompany/thewalkingtec/TheWalkingTec.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.thewalkingtec;
+
+/**
+ *
+ * @author Alina
+ */
+public class TheWalkingTec {
+
+    public static void main(String[] args) {
+        System.out.println("Hola!");
+    }
+}
