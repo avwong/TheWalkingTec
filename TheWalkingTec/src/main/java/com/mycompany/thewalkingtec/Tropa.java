7b5de9752f512e83c8d3108d4f016e4bced4d663
avwong <alinarodriguezwong@gmail.com>
2025-10-14 00:28:28 -0600

    Empezar clase abstracta tropa

---
+++ TheWalkingTec/src/main/java/com/mycompany/thewalkingtec/Tropa.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.thewalkingtec;
+
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author Alina
+ */
+public abstract class Tropa {
+    
+    private String nombre;
+    private ImageIcon apariencia;
+    private int vida;
+    private int danoPorSegundo;
+    private int nivel;
+    private int alcance;
+    private int nivelDeAparicion;
+    //TODO: private Pantalla refPantalla;
+    
+    
+}
